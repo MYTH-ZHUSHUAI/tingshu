adb751f6d7dcb889fda94d54b5011e2aca7e6844
zhushuai <1347250485@qq.com>
2026-05-29 13:49:00 +0800

    feat(common): 添加登录注解并移除MD5工具类中的测试代码
    - 新增Login注解用于方法级别的登录验证
    - 移除MD5工具类中的main测试方法
    - 清理MD5加密相关的测试代码片段

---
+++ common/common-util/src/main/java/com/atguigu/tingshu/common/login/Login.java
@@ -0,0 +1,28 @@
+package com.atguigu.tingshu.common.login;
+
+/*
+ *@auther:zhushuai
+ *@verson 1.0
+ *@2026/5/29 11:22
+ */
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Login {
+
+
+
+
+
+
+
+
+
+
+
+}
--- common/common-util/src/main/java/com/atguigu/tingshu/common/util/MD5.java
+++ common/common-util/src/main/java/com/atguigu/tingshu/common/util/MD5.java
@@ -28,12 +28,4 @@ public final class MD5 {
             throw new RuntimeException("MD5加密出错！！+" + e);
         }
     }
-
-    public static void main(String[] args) {
-        String msg = "{\"liveRoomId\":1,\"msgContent\":1,\"msgType\":\"4\",\"timestamp\":1679969941853}";
-        System.out.println(msg);
-        msg = msg.replaceAll("\\\\", "");
-        System.out.println(msg);
-        System.out.println(MD5.encrypt("111111"));
-    }
 }
